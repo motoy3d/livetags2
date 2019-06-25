ca8b464c1a77ae48ca590edcb31dba62db87060b
motoy3d <motoy3d@gmail.com>
2019-06-26 08:13:57 +0900

    Initial Commit

---
+++ livetags2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  livetags2
+//
+//  Created by Motoi Kataoka on 2019/06/26.
+//  Copyright © 2019 Motoi Kataoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
